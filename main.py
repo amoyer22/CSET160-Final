d283d0a745bd99f411b925285ebbe770174fc2cb
Ailish <snickrrdoodles@gmail.com>
2025-03-26 11:01:37 -0400

    Added page to create tests, adjusted routes and css

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/tests-home')
+@app.route('/tests')
 def testshome():
     return render_template('tests_home.html')
+
+@app.route('/tests/create')
+def testscreate():
+    return render_template('tests_create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
