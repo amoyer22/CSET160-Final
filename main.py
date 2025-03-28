8b54fd766c5b4fe7f63d3a5d5aac1ababb401796
Ailish <snickrrdoodles@gmail.com>
2025-03-28 10:00:28 -0400

    Updated navigation, moved files around for better navigation in home pages

--- main.py
+++ main.py
@@ -60,21 +60,9 @@ def accounts():
     result = conn.execute(text("SELECT * FROM users")).all()
     return render_template('acc.html', users=result)
 
-@app.route('/tests')
-def testshome():
-    return render_template('tests_home.html')
-
 @app.route('/tests/create')
 def testscreate():
     return render_template('tests_create.html')
-
-@app.route('/tests/edit')
-def testsedit():
-    return render_template('tests_edit.html')
-
-@app.route('/tests/delete')
-def testsdelete():
-    return render_template('tests_delete.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
