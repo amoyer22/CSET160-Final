81a3037f2b05be1c47351db3d0b51a2be878fc85
Ailish <snickrrdoodles@gmail.com>
2025-03-28 09:12:16 -0400

    Added error and success messages to sign up and login pages

--- main.py
+++ main.py
@@ -12,33 +12,40 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    message = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        result = conn.execute(text("SELECT * FROM users WHERE username = :username AND password = :password"),
-                              {"username": username, "password": password}).first()
-        user_type = result[3]
-        if result:
-            if user_type == 'teacher':
-                return redirect('/home/teachers')
-            elif user_type == 'student':
-                return redirect('/home/students')
-        else:
-            return "Invalid entry. Try again."
-    return render_template('login.html')
+        try:
+            result = conn.execute(text("SELECT * FROM users WHERE username = :username AND password = :password"),
+                                {"username": username, "password": password}).first()
+            user_type = result[3]
+            if result:
+                if user_type == 'teacher':
+                    return redirect('/home/teachers')
+                elif user_type == 'student':
+                    return redirect('/home/students')
+            else:
+                message = "ERROR: Invalid entry. Try again."
+        except:
+            message = "ERROR: Invalid entry. Try again."
+    return render_template('login.html', message=message)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    message = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         user_type = request.form['type']
-        conn.execute(text("INSERT INTO users (username, password, type) VALUES (:username, :password, :type)"),
-                     {"username": username, "password": password, "type": user_type})
-        conn.commit()
-        return "Sign-up successful."
-    return render_template('signup.html')
+        try:
+            conn.execute(text("INSERT INTO users (username, password, type) VALUES (:username, :password, :type)"),
+                        {"username": username, "password": password, "type": user_type})
+            conn.commit()
+            message = "Sign up successful."
+        except:
+            message = "ERROR: Sign up failed. Try again."
+    return render_template('signup.html', message=message)
 
 @app.route('/home/teachers')
 def teacherhome():
